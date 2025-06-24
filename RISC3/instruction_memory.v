020931e7523bb2cec62ea15bad93f58ec90e8312
Paul Hamshere <p.w.hamshere@gmail.com>
2025-06-24 11:01:43 +0100

    Add files via upload
    New version that has word aligned instruction memory, rather than byte aligned in the processor but word aligned in  instruction_memory.v

---
+++ RISC3/instruction_memory.v
@@ -0,0 +1,24 @@
+`include "parameter.v"
+// fpga4student.com 
+// FPGA projects, VHDL projects, Verilog projects 
+// Verilog code for RISC Processor 
+// Verilog code for Instruction Memory
+
+
+module Instruction_Memory(
+  input  [15:0] pc,
+  output [15:0] instruction
+  );
+
+  reg [`col - 1:0] memory [`row_i - 1:0];
+  //wire [3 : 0] rom_addr = pc[4 : 1];
+  wire [3 : 0] rom_addr = pc[3 : 0];
+  
+  initial
+    begin
+      $readmemb("test_prog.mem", memory, 0, 14);
+    end
+  
+  assign instruction =  memory[rom_addr]; 
+
+endmodule
