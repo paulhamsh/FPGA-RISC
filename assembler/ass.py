e6345000a40a016bc5d272c10a16fa9beeff81a0
Paul Hamshere <p.w.hamshere@gmail.com>
2025-07-16 17:48:34 +0100

    Add files via upload

--- assembler/ass.py
+++ assembler/ass.py
@@ -9,15 +9,13 @@ def is_int(s):
     return s.isnumeric() or (s[0] == "-" and s[1:].isnumeric())
 
 # pretty print the machine code binary, add a line number if not None
-def show_machine_code(line_no, val, c):
+def machine_code_to_str(val):
     v1 = (val & 0b1111_000_000_000_000) >> 12
     v2 = (val & 0b0000_111_000_000_000) >> 9
     v3 = (val & 0b0000_000_111_000_000) >> 6
     v4 = (val & 0b0000_000_000_111_000) >> 3
     v5 = (val & 0b0000_000_000_000_111)
-    if line_no != None:
-        print("{:4d} : ".format(line_no), end="") 
-    print("{:04b}_{:03b}_{:03b}_{:03b}_{:03b} {:s}".format(v1, v2, v3, v4, v5, c))
+    return ("{:04b}_{:03b}_{:03b}_{:03b}_{:03b}".format(v1, v2, v3, v4, v5))
           
 def tokenise(txt) :	
     txt = txt.replace("[", " ")
@@ -27,9 +25,9 @@ def tokenise(txt) :
     txt = txt.lower()
 
     comment = ""
-    comment_location = txt.find("#")
+    comment_location = txt.find("//")
     if comment_location != -1:
-        comment = "//" + txt[comment_location + 1:]
+        comment = "//" + txt[comment_location + 2:]
         txt = txt[:comment_location]
     
     # as a byproduct of the split
@@ -97,8 +95,10 @@ def tokenise(txt) :
 	
 
 def assemble(code):
-    full_machine_code = []
-    labels = {}
+    result = []
+    label_to_line = {}
+    line_to_label = {}
+    
     arith_cmds = {"add": 2, "sub": 3, "lsl": 5,
                   "lsr": 6, "and": 7, "or" : 8,
                   "slt": 9}
@@ -108,7 +108,8 @@ def assemble(code):
     for line in code:
         (label, cmd, regA, regB, regC, value, jmp_label, comment) = tokenise(line)
         if label:
-            labels[label] = line_number
+            label_to_line[label] = line_number
+            line_to_label[line_number] = label
         if cmd:
             line_number += 1
 
@@ -116,20 +117,21 @@ def assemble(code):
     line_number = 0
     for line in code:
         (label, cmd, regA, regB, regC, value, jmp_label, comment) = tokenise(line)
-        #print(">>", label, cmd, regA, regB, regC, value, jmp_label)
-        machine_code = 0
+        machine_code = None
     
         # Calculate a jump offset
         if jmp_label:
             if cmd == "jmp":
-                value = labels[jmp_label]
-            else:           # only do this if we are bne or beq
-                value = labels[jmp_label] - (line_number + 1)
+                value = label_to_line[jmp_label]
+            else:
+                # only do this if we are bne or beq
+                value = label_to_line[jmp_label] - (line_number + 1)
 
         # adjust the offset if negative
         if value:
             if value < 0:
                 value = 64 + value
+
         if cmd:
             if   cmd == "ld":
                 machine_code = 0b0000 << 12
@@ -172,10 +174,19 @@ def assemble(code):
                 machine_code |= regC << 6	
                 machine_code |= regA << 3
 
-            line_number += 1
-            full_machine_code.append((machine_code, comment))
 
-    return full_machine_code
+        if line_to_label.get(line_number):
+            label = line_to_label[line_number]
+        else:
+            label = None
+            
+        if machine_code or comment:
+            # don't add if just a label, that will still be there for the line with a command
+            result.append((line_number, label, machine_code, comment))
+
+        if machine_code:
+            line_number += 1
+    return result
 
 ##############################################################
 
@@ -200,83 +211,38 @@ fmc = assemble(code_clean)
 # Print out the result
 for l in code_clean:
     print(l)
-
 print()
 
-line_no = 0
-for v, c in fmc:
-    show_machine_code(line_no, v, c)
-    line_no += 1
 
+def create_line(do_line_numbers, line_no, label, code, comment):
+    s = ""
+    if label and code:
+        s += ("// [{:s}:{:d}] \n".format(label, line_no)) + s
+    if code:
+        if do_line_numbers:
+            s += "{:4d} : ".format(line_no)
+        s += machine_code_to_str(code) + " "
+    if code and comment:
+        s += " "
+    if comment:
+        s += comment
+    if code or comment:
+        return s
+    else:
+        return None
+
+
+# Print machine code with line numbers
+for line_no, label, code, comment in fmc:
+    s = create_line(True, line_no, label, code, comment)
+    if s:
+        print(s)
+
+# Print machine code to a file
 if outname:
     f = open(outname, mode='w')
-    for v, c in fmc:
-        print("{:016b} {:s}".format(v, c), file=f)
+    for line_no, label, code, comment in fmc:
+        s = create_line(False, line_no, label, code, comment)
+        if s:
+            print(s, file = f)
     f.close()
-
-##############################################################
-
-"""
-
-code2 = ("# Comment line",                
-         "",                              
-         "start:",                        # =0
-         "ld  r1, r2[5] # comment",       #0
-         "st  r3, r5[3] # this is",       #1
-         "add r4, r2, r1 # a ",           #2
-         "interim:",                      # =3
-         "sub r7, r3, r0 # comment",      #3
-         "lsl r6, r4, r1 ",               #4
-         "lsr r5, r5, r2",                #5
-         "slt r4, r6, r3",                #6
-         "and r3, r7, r4",                #7
-         "or  r2, r5, r6",                #8
-         "inv r1, r2",                    #9
-         "beq r1, r2, start",             #10
-         "bne r1, r2, end",               #11
-         "jmp interim",                   #12
-         "end:"                           # =13
-         )
-         
-code = ("# Comment line",
-        "first_label:",
-        "label: LD r1, r2, r3[-1]",
-        "lab_me:\n\tLD r1, r2, (r3+-1) # weird to have +- but it has to be that way",
-        "ADD r1, r2, r3",	
-        "INV r1, r2, r3",	
-        "BNE r1, r0, -1",	
-        "lab2: LD r1, r2, r3[31]",
-        "lab3: jmp lab3",	
-        "lab4: jmp 20",	
-	"BNE r1, r2, label",
-        "BNE r1, r2, +5"
-       )	
-
-code = ("# Comment line",                
-        "",                              
-        "start:",                        # =0
-        "ld  r1, r2[5] # comment",       #0
-        "st  r3, r5[3] # this is",       #1
-        "add r4, r2, r1 # a ",           #2
-        "interim:",                      # =3
-        "sub r7, r3, r0 # comment",      #3
-        "lsl r6, r4, r1 ",               #4
-        "lsr r5, r5, r2",                #5
-        "slt r4, r6, r3",                #6
-        "and r3, r7, r4",                #7
-        "or  r2, r5, r6",                #8
-        "inv r1, r2",                    #9
-        "beq r1, r2, start",             #10
-        "bne r1, r2, end",               #11
-        "jmp interim",                   #12
-        "end:"                           # =13
-        )
-
-
-for val in range (-32, 32):
-    if val < 0:
-        new_val = 64 + val
-    else:
-        new_val = val
-    print ("{:4d} {:08b}". format(val, new_val))
-"""
